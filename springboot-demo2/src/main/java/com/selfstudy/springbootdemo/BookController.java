a8bad531c34a925ebb96b88e06101f0ca08a5ac8
Naiyou <nyliu80@gmail.com>
2020-06-21 12:07:00 -0500

    -first commit

---
+++ springboot-demo2/src/main/java/com/selfstudy/springbootdemo/BookController.java
@@ -0,0 +1,19 @@
+package com.selfstudy.springbootdemo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+		
+		@GetMapping("/books")
+		public List<Book> getAllBooks () {
+			return Arrays.asList(new Book(1l, "plant embryology 3", "naiyou"));
+		}
+
+	}
+
+
